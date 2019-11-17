57a97ccb35e1a59c2846b8846b8a545fe6b91d2f
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2019-11-17 11:03:12 -0300

    Performed some PEP8 fixes.

--- PythonInterface/tiff_process.py
+++ PythonInterface/tiff_process.py
@@ -3,14 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 
-def tiff_to_dataframe(path):
+
+def tiff_to_dataframe(input_path, output_path=None, image_output=None):
     # Arrays used to setup Dataframe
     x_coordinates = []
     y_coordinates = []
     im_value = []
 
     # Creating the numpy array from image
-    im = Image.open(path)
+    im = Image.open(input_path)
     im = np.array(im)
 
     # Plot image using a "hot" color scale before processing
@@ -29,49 +30,55 @@ def tiff_to_dataframe(path):
     # plt.colorbar()
     # plt.show()
 
-    Image.fromarray(im).save('./cps_df.tif')
+    # Saves image to specified path
+    if image_output is not None:
+        Image.fromarray(im).save(image_output)
 
-    # Separates the arrays
-    for i in range(len(im[0])):
-        for j in range(len(im[:,0])):
-            im_value.append(im[j,i])
-            x_coordinates.append(j)
-            y_coordinates.append(i)
+    # Generates dataframe
+    elif output_path is not None:
+        # Separates the arrays
+        for i in range(len(im[0])):
+            for j in range(len(im[:, 0])):
+                im_value.append(im[j, i])
+                x_coordinates.append(j)
+                y_coordinates.append(i)
 
-    # Converts numpy array to dataframe.
-    x_array = np.array(x_coordinates)
-    y_array = np.array(y_coordinates)
-    values_array = np.array(im_value)
+        # Converts numpy array to dataframe.
+        x_array = np.array(x_coordinates)
+        y_array = np.array(y_coordinates)
+        values_array = np.array(im_value)
 
-    dataframe = pd.DataFrame({'x':x_array, 'y':y_array, 'value':values_array})
-    return dataframe
+        dataframe = pd.DataFrame({'x': x_array, 'y': y_array, 'value': values_array})
+        dataframe.to_csv(path_or_buf=output_path, index=False, header=False, sep=';')
+
+        return dataframe
+
 
 def find_near_null(image):
     min_value = float("inf")
     for i in range(len(image[0])):
-        for j in range(len(image[:,0])):
-            if image[j,i] < min_value:
-                min_value = image[j,i]
+        for j in range(len(image[:, 0])):
+            if image[j, i] < min_value:
+                min_value = image[j, i]
     
     return min_value
 
+
 def remove_near_null(im, near_null):
     img = im[~np.all(im == near_null, axis=1)]
-    img = img[:,~np.all(img == near_null, axis=0)]
+    img = img[:, ~np.all(img == near_null, axis=0)]
     # Swaps the near null values to None
     for i in range(len(img[0])):
-        for j in range(len(img[:,0])):
-            if img[j,i] == near_null:
-                img[j,i]= None
+        for j in range(len(img[:, 0])):
+            if img[j, i] == near_null:
+                img[j, i] = 0.0
     
     return img
 
+
 if __name__ == "__main__":
     # Main apenas para debug
-    image_path = "/Users/rafaelprado/Downloads/cps_df.tif"
-    output_path = "/Users/rafaelprado/Code/ImmMaps/MapPlot/Assets/input/_input.csv"
-    print("Starting image processing...")
-    df = tiff_to_dataframe(image_path)
-    print("Image converted to dataframe.\nNow dumping output file...")
-    df.to_csv(path_or_buf=output_path, index=False, header=False, sep=';')
-    print("Dumping complete!")
+    img_path = "/Users/rafaelprado/Downloads/cps_df.tif"
+    out_path = "/Users/rafaelprado/Desktop/immmaps.app/Contents/_input.csv"
+    img_output = "/Users/rafaelprado/Desktop/mask_cps2.tif"
+    tiff_to_dataframe(img_path, out_path, img_output)
