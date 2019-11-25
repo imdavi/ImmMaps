6d282cc2354ed9982e9944e024f66fa66fdbeb3c
Rafael Tomaz Prado <rafael.tomaz.prado@gmail.com>
2019-11-24 22:37:57 -0300

    Fixed the cropping method to actually remove data from outside the bounds of the shape. Also removes the non-value points from the image to reduce size. Finally, calculates the heightmap to transmit to Unity.

--- PythonInterface/gis_manager.py
+++ PythonInterface/gis_manager.py
@@ -1,40 +1,62 @@
 import geopandas as gpd
 import rasterio as rio
-import earthpy.spatial as es
 import numpy as np
+import os
+import gdal
+
+temp_path = os.path.dirname(os.path.abspath(__file__)) + "/_temp/"
 
 
-def crop(image_file, shape_file, return_meta=False):
-    # Loads the SHP and the image file
-    crop_frontier = gpd.read_file(shape_file)
+def crop(image_file, shape_file, heightmap=False):
+    os.makedirs(temp_path, exist_ok=True)
+    output_temp = temp_path+"result.tif"
+
+    crop_bound = shape_file
+
+    shape = gpd.read_file(shape_file)
     image = rio.open(image_file)
 
-    # Checks if image has coordinate system
-    if image.crs is None:
-        raise Exception("Image file must have a coordinate system.")
-    # Acquires the image coordinate system to apply to the dataframe
-    image_crs = image.crs.data['init'].split(':')[1]
+    if not shape.crs == image.crs and image.crs is not None:
+        reproj_shp_path = temp_path + "_temp_reproj.shp"
 
-    # Apply image CRS to SHP
-    crop_frontier = crop_frontier.to_crs(epsg=image_crs)
-    # Crops the image, masking with the SHP
-    data, meta = es.crop_image(image, crop_frontier)
+        shape.to_crs(crs=image.crs['init'], inplace=True)
+        shape.to_file(reproj_shp_path)
+        crop_bound = reproj_shp_path
 
-    if return_meta:
-        return data, meta
-    else:
-        return data[0]
+    gdal.Warp(output_temp, image_file, cutlineDSName=crop_bound)
+
+    with rio.open(output_temp) as src:
+        cropped = src.read()
+
+    _clear_temp()
+
+    result = _remove_values(cropped, cropped.min())
+
+    if heightmap:
+        return generate_heightmap(result)
+
+    return result
 
 
-def remove_values(im, near_null):
-    img = im[~np.all(im == near_null, axis=1)]
+def _remove_values(im, near_null):
+    try:
+        img = im[~np.all(im == near_null, axis=1)]
+    except:
+        img = im[~np.all(im == near_null, axis=2)]
+
     img = img[:, ~np.all(img == near_null, axis=0)]
-    # Swaps the near null values to None
+    # Swaps the near null values to NaN
     for i in range(len(img[0])):
         for j in range(len(img[:, 0])):
             if img[j, i] == near_null:
                 img[j, i] = np.nan
     return img
+
+
+def _clear_temp():
+    for file in os.listdir(temp_path):
+        os.remove(temp_path+file)
+    os.removedirs(temp_path)
 
 
 def generate_heightmap(array):
@@ -44,6 +66,3 @@ def generate_heightmap(array):
 
     normalize = np.vectorize(lambda i: i/ceiling)
     return normalize(array)
-
-
-
